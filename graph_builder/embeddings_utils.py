643ac3213670aca8d5662bd448d7828131210bfd
einszwaidrei <97828035+einszwaidrei@users.noreply.github.com>
2025-06-02 00:36:15 +0500

    Update embeddings_utils.py

--- graph_builder/embeddings_utils.py
+++ graph_builder/embeddings_utils.py
@@ -1,6 +1,6 @@
 from sentence_transformers import SentenceTransformer
 
-embedding_model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
+embedding_model = SentenceTransformer('rubert-base-sentence')
 
 def triplet_to_text(triplet: dict) -> str:
     return f"{triplet['subject']} {triplet['predicate']} {triplet['object']}"
